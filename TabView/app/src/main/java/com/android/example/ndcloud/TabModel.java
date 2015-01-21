40cb9c44e889be3b5fea737472207ab3e6e00433
Bing <liubing2004@gmail.com>
2015-01-20 23:09:56 -0800

    add tabview

---
+++ TabView/app/src/main/java/com/android/example/ndcloud/TabModel.java
@@ -0,0 +1,15 @@
+package com.android.example.ndcloud;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TabModel extends Activity {
+
+    /** Called when the activity is first created. */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabmodel);
+    }
+}
---
+++ TabView/app/src/main/java/com/android/example/ndcloud/TabUser.java
@@ -0,0 +1,17 @@
+package com.android.example.ndcloud;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bing on 1/19/15.
+ */
+public class TabUser extends Activity {
+    /** Called when the activity is first created. */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabuser);
+    }
+}
